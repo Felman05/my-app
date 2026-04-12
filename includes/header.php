3d173ba163818d69447523a382652bd6ad6d4460
Linux Adona <linuxadona17@gmail.com>
2026-04-12 19:48:54 +0800

    Sinama ko fontawesome icons para malinis tignan sidebar

--- includes/header.php
+++ includes/header.php
@@ -11,6 +11,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,300;0,9..144,700;1,9..144,700&family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600&display=swap" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/doon-app/assets/css/main.css">
     <?php if (isset($additionalCSS)) echo $additionalCSS; ?>
     <?php if (!empty($loadGoogleMapsApi) && !empty($googleMapsKey)): ?>
